6d033e0fc3bac73d89c26ec931ce610f96580d21
vrsssrikanth-glitch <vrsssrikanth@gmail.com>
2025-12-25 22:02:19 +0530

    Update time_to_target.py

--- time_to_target.py
+++ time_to_target.py
@@ -40,9 +40,9 @@ def estimate_time_to_target_empirical(df, target_mult=2, lookback=250):
          future = df.iloc[i + 1 : i + 31]
          hit = future[future["High"] >= target]
 
-       if not hit.empty:
-         days = future.index.get_loc(hit.index[0]) + 1
-         times.append(days)
+         if not hit.empty:
+           days = future.index.get_loc(hit.index[0]) + 1
+           times.append(days)
 if not times:
    return None
 
